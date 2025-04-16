78ea276b2ae675d4252fe1a0cfeb9ebecac81b3c
Victoria Andrade <vandradev2@est.ups.edu.ec>
2025-04-16 08:23:13 -0500

    Busqueda Binaria

--- src/App.java
+++ src/App.java
@@ -1,11 +1,12 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 import models.Persona;
+import controllers.BusquedaBinaria;
 import controllers.MetodoBusqueda;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-
+        
         Persona [] personas = new Persona[7];
         personas [0] = new Persona (101, "Juan");
         personas [1] = new Persona (102, "Maria");
@@ -15,9 +16,16 @@ public class App {
         personas [5] = new Persona (106, "Sofia");
         personas [6] = new Persona (107, "Pedro");
 
-        MetodoBusqueda metodoBusqueda = new MetodoBusqueda(personas);
+        //MetodoBusqueda metodoBusqueda = new MetodoBusqueda(personas);
+
+        BusquedaBinaria busquedaBinaria = new BusquedaBinaria(personas);
+
+        busquedaBinaria.showPersonByCode();
+
+        /*
         Scanner scanner = new Scanner(System.in);
-        int[] arreglo = {10, 15, 23, 56, 19};
+        int[] arreglo = {10, 15, 23, 56, 19, 45};
+
         
         System.out.println("Arreglo original");
         for(int i = 0; i < arreglo.length; i++){
@@ -29,14 +37,17 @@ public class App {
 
         System.out.println("Ingrese el numero que desea buscar");
         numero = scanner.nextInt();
+
+
+        /*Busqueda
         posicion = metodoBusqueda.busquedaLieal(arreglo, numero);
 
         if(posicion == -1){
             System.out.println("No se ha encontrado el elemento");
         }
         System.out.println("El numero ->" + numero + "se encuentra en la posicion ->" + (posicion + 1));
-
-
-        scanner.close();
+        
+        scanner.close();*/
+    
     }
 }
---
+++ src/controllers/BusquedaBinaria.java
@@ -0,0 +1,52 @@
+package controllers;
+import models.Persona;
+import views.ShowConsole;
+
+public class BusquedaBinaria {
+
+    private Persona[] people; //variable global, arreglo de personas 
+    private ShowConsole showConsole; //creamos una variable tipo privada de la clase ShowConsole
+
+    public BusquedaBinaria (Persona[] personas){
+        this.people = personas; // instanciada en el constructor, la instancia se hace con el valor que llega como argumento
+        
+        this.showConsole = new ShowConsole();
+        showConsole.showMesagge("Metodos de Busqueda Binaria");
+   
+    }
+
+    public int findPersonByCode(int code){
+        int bajo = 0;
+        int alto = (people.length -1);
+
+        while(bajo <= alto){
+            int central = bajo + (alto - bajo)/2;
+            //int central =
+
+            if(people[central].getCode() == code){
+                return central;
+            }
+
+            if(people[central].getCode() < code){ //nos vamos a la derecha, se actualiza el bajo
+                bajo = central + 1;
+            }else {
+                alto = central - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public void showPersonByCode(){
+        int codeToFind = showConsole.getCode();
+        int indexPerson = findPersonByCode(codeToFind);
+
+        if(indexPerson == -1) {
+            showConsole.showMesagge("No hay la persona con el codigo");
+
+        }else{
+            showConsole.showMesagge("Persona con codigo " + codeToFind+ "  encontrada");
+            showConsole.showMesagge(people[indexPerson].toString());
+        }
+    } 
+}
